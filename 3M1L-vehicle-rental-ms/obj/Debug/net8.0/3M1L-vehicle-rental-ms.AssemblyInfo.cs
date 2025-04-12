b2004a96760897f2db71795b49851d930ca22bfb
mark-k-wg <markk@wacadsgroup.com>
2025-04-12 11:01:03 -0600

    Update CustomerController

--- 3M1L-vehicle-rental-ms/obj/Debug/net8.0/3M1L-vehicle-rental-ms.AssemblyInfo.cs
+++ 3M1L-vehicle-rental-ms/obj/Debug/net8.0/3M1L-vehicle-rental-ms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3M1L-vehicle-rental-ms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9eec35189326681f49da829908bcfa1e6325e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb87ea78c59fc08579ea477740bdfeba34ae962")]
 [assembly: System.Reflection.AssemblyProductAttribute("3M1L-vehicle-rental-ms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3M1L-vehicle-rental-ms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
