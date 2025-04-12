bcb87ea78c59fc08579ea477740bdfeba34ae962
mark-k-wg <markk@wacadsgroup.com>
2025-04-12 10:23:48 -0600

    Update mock database

--- 3M1L-vehicle-rental-ms/Data/RentalDbContext.cs
+++ 3M1L-vehicle-rental-ms/Data/RentalDbContext.cs
@@ -3,6 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace _3M1L_vehicle_rental_ms.Data
 {
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+
     public class RentalDbContext:DbContext
     {
         public RentalDbContext(DbContextOptions options):base(options)
@@ -10,8 +18,9 @@ namespace _3M1L_vehicle_rental_ms.Data
             
         }
 
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
-        public DbSet<Vehicle> Customers { get; set; }
         public DbSet<Vehicle> Reports { get; set; }
     }
 }
