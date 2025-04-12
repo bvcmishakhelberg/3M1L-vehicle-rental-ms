0f70340cd0c71e688faa47c5b4ce07d8f92a54dd
mark-k-wg <markk@wacadsgroup.com>
2025-04-12 11:17:35 -0600

    Add user model, login view, and register view

--- 3M1L-vehicle-rental-ms/Data/RentalDbContext.cs
+++ 3M1L-vehicle-rental-ms/Data/RentalDbContext.cs
@@ -3,14 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace _3M1L_vehicle_rental_ms.Data
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public required string Username { get; set; }
-        public required string Email { get; set; }
-        public required string PasswordHash { get; set; }
-    }
-
     public class RentalDbContext:DbContext
     {
         public RentalDbContext(DbContextOptions options):base(options)
---
+++ 3M1L-vehicle-rental-ms/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace _3M1L_vehicle_rental_ms.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
