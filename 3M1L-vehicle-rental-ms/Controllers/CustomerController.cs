666a47f78ba2d3be91ba230ed2713761cc9acc68
mark-k-wg <markk@wacadsgroup.com>
2025-04-12 11:01:37 -0600

    Update CustomerController

--- 3M1L-vehicle-rental-ms/Controllers/CustomerController.cs
+++ 3M1L-vehicle-rental-ms/Controllers/CustomerController.cs
@@ -1,106 +1,84 @@
-﻿//using _3M1L_vehicle_rental_ms.Models;
-//using _3M1L_vehicle_rental_ms.Data;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
+﻿using _3M1L_vehicle_rental_ms.Data;
+using _3M1L_vehicle_rental_ms.Models;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace _3M1L_vehicle_rental_ms.Controllers
-//{
-//    public class CustomerController : Controller
-//    {
-//        private readonly RentalDbContext RentalDbContext;
+namespace _3M1L_vehicle_rental_ms.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly RentalDbContext _context;
 
+        public CustomerController(RentalDbContext context)
+        {
+            _context = context;
+        }
 
-//        public CustomerController(RentalDbContext RentalDbContext)
-//        {
-//            this.RentalDbContext = RentalDbContext;
-//        }
+        // GET: /Customer
+        public IActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
 
-//        // Get all jokes
-//        [HttpGet]
-//        [Route("Joke")]
-//        public async Task<IActionResult> Index()
-//        {
-//            var jokes = await RentalDbContext.Jokes.ToListAsync();
-//            return View(jokes);
-//        }
+        // GET: /Customer/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-//        // Get one joke
-//        [HttpGet]
-//        [Route("Joke/Details/{id}")]
-//        public async Task<IActionResult> Details(int id)
-//        {
-//            var joke = await RentalDbContext.Jokes.FirstOrDefaultAsync(a => a.Id == id);
-//            return View(joke);
-//        }
+        // POST: /Customer/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
 
-//        // Get the Add.cshtml page
-//        [HttpGet]
-//        [Route("Joke/Add")]
-//        public IActionResult Add()
-//        {
-//            return View();
-//        }
+        // GET: /Customer/Edit/{id}
+        public IActionResult Edit(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null) return NotFound();
+            return View(customer);
+        }
 
-//        // Create a new joke
-//        [HttpPost]
-//        [Route("Joke/Add")]
-//        public async Task<IActionResult> Add(Vehicle joke)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                var newjoke = new Vehicle()
-//                {
-//                    JokeQuestion = joke.JokeQuestion,
-//                    JokeAnswer = joke.JokeAnswer,
-//                };
-//                await RentalDbContext.Jokes.AddAsync(newjoke);
-//                await RentalDbContext.SaveChangesAsync();
-//                return RedirectToAction("Index");
-//            }
-//            return View(joke);
-//        }
+        // POST: /Customer/Edit/{id}
+        [HttpPost]
+        public async Task<IActionResult> Edit(Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Update(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
 
-//        // Get Edit.cshtml page
-//        [HttpGet]
-//        [Route("Joke/Edit/{id}")]
-//        public async Task<IActionResult> Edit(int id)
-//        {
-//            var joke = await RentalDbContext.Jokes.FirstOrDefaultAsync(a => a.Id == id);
-//            return View(joke);
-//        }
-        
-//        // Edit one joke
-//        [HttpPost]
-//        [Route("Joke/Edit/{id}")]
-//        public async Task<IActionResult> Edit(Vehicle NewJoke)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                var joke = await RentalDbContext.Jokes.FindAsync(NewJoke.Id);
-//                if (joke != null)
-//                {
-//                    joke.JokeQuestion = NewJoke.JokeQuestion;
-//                    joke.JokeAnswer = NewJoke.JokeAnswer;
-//                    await RentalDbContext.SaveChangesAsync();
-//                    return RedirectToAction("Index");
-//                }
-                
-//            }
-//            return View(NewJoke);
-//        }
+        // GET: /Customer/Delete/{id}
+        public IActionResult Delete(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null) return NotFound();
+            return View(customer);
+        }
 
-//        // Delete
-//        [HttpGet]
-//        [Route("Joke/Delete/{id}")]
-//        public async Task<IActionResult> Delete(int id)
-//        {
-//            var joke = await RentalDbContext.Jokes.FindAsync(id);
-//            if (joke != null)
-//            {
-//                RentalDbContext.Jokes.Remove(joke);
-//                await RentalDbContext.SaveChangesAsync();
-//            }
-//            return RedirectToAction("Index");
-//        }
-//    }
-//}
+        // POST: /Customer/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
